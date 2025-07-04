7406718c348eeea5fe05f6e44b1ece34a348dc81
Matheus Fellet <matheusmfellet@gmail.com>
2025-07-04 10:53:49 -0300

    add: Adicionando funções helpers

---
+++ scr/helpers.py
@@ -0,0 +1,42 @@
+import unicodedata
+
+from models import Usuario, Discente, Docente, Funcionario
+
+
+def clean_string(string):
+    clean_str = (
+        unicodedata.normalize('NFKD', string).encode('ascii', 'ignore').decode('ascii')
+    ).lower()
+
+    return clean_str
+
+
+def get_user(cpf, type_usr):
+    if type_usr == 'discente':
+        return Discente.query.filter_by(cpf=cpf).first()
+    elif type_usr == 'docente':
+        return Docente.query.filter_by(cpf=cpf).first()
+    elif type_usr == 'funcionario':
+        return Funcionario.query.filter_by(cpf=cpf).first()
+    return None
+
+
+def verify_credentials_sign_up(cpf):
+    user = Usuario.query.filter_by(cpf=cpf).first()
+
+    if user:
+        return False
+
+    return True
+
+
+def verify_credentials_login(email, senha, type_usr):
+    user = Usuario.query.filter_by(email=email, senha=senha).first()
+    cpf = user.cpf  # type: ignore
+
+    specific_user = get_user(cpf, type_usr)
+
+    if user and specific_user:
+        return user
+
+    return False
