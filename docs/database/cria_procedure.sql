0a491ca5ca04d5d1ebf44070556bc142c98a2e13
Bruno Eduardo <102605344+bduardol@users.noreply.github.com>
2025-07-08 20:09:39 -0300

    melhorando procedure

--- docs/database/cria_procedure.sql
+++ docs/database/cria_procedure.sql
@@ -2,10 +2,11 @@ CREATE OR REPLACE PROCEDURE excluir_ocorrencia_por_id(id_oc INTEGER)
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    DELETE FROM ocorrencia WHERE id = id_oc;
+    DELETE FROM ocorrencia 
+    WHERE id = id_oc AND data_fechamento IS NULL;
 
     IF NOT FOUND THEN
-        RAISE NOTICE 'Ocorrência com ID % não encontrada.', id_oc;
+        RAISE NOTICE 'Ocorrência com ID % não encontrada ou já foi fechada.', id_oc;
     ELSE
         RAISE NOTICE 'Ocorrência com ID % excluída com sucesso.', id_oc;
     END IF;
